37c0cf1d6e798fb8774f2f2a657f862e1b3f0c78
Emiliano <54007647+Villiem@users.noreply.github.com>
2019-09-12 12:36:56 -0500

    Update Dofile gapminder.R

--- Dofile gapminder.R
+++ Dofile gapminder.R
@@ -21,12 +21,21 @@ gapminder %>% select (country, pop, year,)
 
 gapminder %>% filter (country == "Mexico")
 gapminder %>% filter(country =="Mexico", year = 2007)
-## gapminder %>% filter(country =="Mexico, year = 2007 year ==1952)
-## usar vectores
+## Si queremos muchos años
+## Completa el comando de abajo con & (and) o | (o)
+## gapminder %>% filter(country =="Mexico, year = 2007 (operador) year ==1952)
+gapminder_df %>% filter (country == "Mexico", year == c(2002, 2007))
+
+
+
+
+## Completa el comando de abajo con & (and) o | (o)
+## gapminder %>% filter(country =="Mexico, year = 2007 (operador) year ==1952)
+
 ## ejercicio crear un df con todos los paises del TLC despues de 1994
 ## Opciones de operadores
-gapminder %>% filter(coutry != "Mexico")
-## usar vectores
+gapminder %>% filter(country != "Mexico")
+
 ## ggplot(data) + geom_line(aes(x=,y=))
 
 gapminder %>% arrange (lifeExp)
@@ -34,8 +43,13 @@ gapminder %>% arrange (lifeExp)
 
 gapminder %>% filter (continent == "Americas", year == 2007) %>% arrange(desc(gdpPercap))
 
-
+## Agrupamos, y creamos una nueva variable llamada media_esperanza que es la media de la esperanza de vida
 gapminder %>% group_by(year, continent) %>% summarise(media_esperanza = mean(lifeExp))
+
+## Que tal si queremos saber la esperanza media de vida de cada continente en cierto anio?
+gapmider %>% group_by (continent) %>% filter (year == 2007) %>% summarise(media_ingreso = mean(gdpPercap)
+                                                                          
+## Lo importante no es memorizar los comandos ni copiar y pegar, es entender que hace cada uno y moldearlo a las necesidades
 
 
 
