ffbbb393ff4bf0eb77513d9c0e571ea50da2b5af
Emiliano <54007647+Villiem@users.noreply.github.com>
2019-09-12 23:27:46 -0500

    Create GraficaGapminder.R

---
+++ GraficaGapminder.R
@@ -0,0 +1,28 @@
+install.packages(c("gapminder","tidyverse","gganimate","png")
+library(gapminder)
+library(tidyverse)
+library(gganimate)
+library(png)
+gapminder <- gapminder
+gapminder2007 <- filter(gapminder, year == 2007)
+ggplot(gapminder2007) + geom_point(aes(x = lifeExp, y = gdpPercap))
+ggplot(gapminder2007) + geom_point(aes(x = gdpPercap, y = lifeExp, color = continent))
+ggplot(gapminder2007) + geom_point(aes(x = gdpPercap, y = lifeExp, color = continent)) + labs(title = "A mayor ingreso mayor esperanza de vida", subtitle = "Para la clase de estadistica", x = "Pib per capita", y = "Esperanza de vida")
+
+
+ggplot(gapminder2007) + geom_point(aes(x = gdpPercap, y = lifeExp, color = continent, size = pop)) + labs(title = "A mayor ingreso mayor esperanza de vida", subtitle = "Para la clase de estadistica", x = "Pib per capita", y = "Esperanza de vida") + theme_classic()
+ggplot(gapminder2007) + aes(x = gdpPercap, y = lifeExp, color = continent, size = pop) + geom_point() + labs(title = "A mayor ingreso mayor esperanza de vida", subtitle = "Para la clase de estadistica", x = "Pib per capita", y = "Esperanza de vida") + theme_classic()
+ggplot(gapminder2007) + aes(x = gdpPercap, y = lifeExp, color = continent, size = pop) + geom_point() + labs(title = "A mayor ingreso mayor esperanza de vida", subtitle = "Para la clase de estadistica", x = "Pib per capita", y = "Esperanza de vida") + theme_classic() + guides (size = FALSE)
+ggplot(gapminder2007) + aes(x = gdpPercap, y = lifeExp, color = continent, size = pop) + geom_point() + labs(title = "A mayor ingreso mayor esperanza de vida", subtitle = "Para la clase de estadistica", x = "Pib per capita", y = "Esperanza de vida") + theme_classic() + guides (size = FALSE) + scale_x_log10()
+ggplot(gapminder2007) + aes(x = gdpPercap, y = lifeExp, color = continent, size = pop) + geom_point() + labs(title = "A mayor ingreso mayor esperanza de vida", subtitle = "Para la clase de estadistica", x = "Pib per capita", y = "Esperanza de vida") + theme_classic() + guides (size = FALSE) + scale_x_log10(breaks = c(400,4000,25000))
+ggplot(gapminder2007) + aes(x = gdpPercap, y = lifeExp, color = continent, size = pop) + geom_point() + labs(title = "A mayor ingreso mayor esperanza de vida", subtitle = "Para la clase de estadistica", x = "Pib per capita", y = "Esperanza de vida") + theme_classic() + guides (size = FALSE) + scale_x_log10(breaks = c(400,4000,25000)) + transition_time(year)
+ggplot(gapminder2007) + aes(x = gdpPercap, y = lifeExp, color = continent, size = pop) + geom_point() + labs(title = "A mayor ingreso mayor esperanza de vida", subtitle = "Para la clase de estadistica", x = "Pib per capita", y = "Esperanza de vida") + theme_classic() + guides (size = FALSE) + scale_x_log10(breaks = c(400,4000,25000)) + transition_time(year) + ease_aes('linear')
+animado<-ggplot(gapminder2007) + aes(x = gdpPercap, y = lifeExp, color = continent, size = pop) + geom_point() + labs(title = "A mayor ingreso mayor esperanza de vida", subtitle = "Para la clase de estadistica", x = "Pib per capita", y = "Esperanza de vida") + theme_classic() + guides (size = FALSE) + scale_x_log10(breaks = c(400,4000,25000)) + transition_time(year) + ease_aes('linear')
+animado
+animado<-ggplot(gapminder) + aes(x = gdpPercap, y = lifeExp, color = continent, size = pop) + geom_point() + labs(title = "A mayor ingreso mayor esperanza de vida", subtitle = "Para la clase de estadistica", x = "Pib per capita", y = "Esperanza de vida") + theme_classic() + guides (size = FALSE) + scale_x_log10(breaks = c(400,4000,25000)) + transition_time(year) + ease_aes('linear')
+animado
+anim_save(filename="clase.gif",
+animation = animado,
+width=2100,
+height=1500,
+res=400)
