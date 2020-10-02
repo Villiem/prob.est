4378e7c6ffb9941d8461ef4c1bced35e774624df
Emiliano <54007647+Villiem@users.noreply.github.com>
2020-10-01 20:29:20 -0500

    Update Funciones.R

--- Introduccion/Funciones.R
+++ Introduccion/Funciones.R
@@ -47,4 +47,75 @@ factoria2 <- function(n){
 factoria2(5)
 
 
+mean
+sample
+
+## Una función básica consta de 3 partes
+# 1. parámetros
+# 2. Cuerpo
+# 3. Regresa
+
+uno_mas_otro <- function(a, b){
+  a + b
+}
+
+uno_mas_otro(1, 2)
+
+uno_a_la_potencia <- function(a, b){
+  a ^ b
+}
+
+## Factorial n! n-1! * n-2! *.... 0
+10! = 10 * 9!
+9! = 9 * 8!
+  
+ 
+factorial2 <- function(a){
+  if(a == 0){
+    return(1)
+  }
+  a * factorial2(a - 1)
+}
+
+operacion1 <- function(a){
+  x <- mean(a)
+  x + 2
+}
+
+x <- c(1,2,3)
+a <- c(4,5,6)
+operacion1(x)
+
+
+chicharronera <- function(a, b, c){
+  x <-  (b ^ 2) - (4 * a * c)
+  z <- 2 * a
+  if (x >= 0) {
+    x1 <- ((-b) + sqrt(x))/z
+    x2 <- ((-b) - sqrt(x))/z
+    return(c(x1, x2))
+  }
+  print('es negativo el resultado')
+}
+
+chicharronera(2,-4,4)
+chicharronera <- function(a,b,c) {
+  p1 <- (b^2) - (4 * a * c)
+  p2 <- 2 * a
+  p3 <- -b
+  if (p1 < 0 ) {
+    print('no tiene solucion real')
+    stop()
+  }
+  x1 <- (p3 + sqrt (p1)) / p2
+  x2 <- (p3 - sqrt (p1)) / p2
+  return (c (x1,x2))
+}
+
+
+
+chicharronera(1, -5, 6)
+chicharronera(2,-4,4)
+
+
 
